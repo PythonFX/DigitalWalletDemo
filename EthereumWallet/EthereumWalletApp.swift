8c029fb875ebecda64fa3d18cb1b14f9b87cc39d
zhaohuiming <zhaohuiming@bytedance.com>
2022-08-13 12:14:29 +0800

    Initial Commit

---
+++ EthereumWallet/EthereumWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  EthereumWalletApp.swift
+//  EthereumWallet
+//
+//  Created by ByteDance on 2022/8/13.
+//
+
+import SwiftUI
+
+@main
+struct EthereumWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
